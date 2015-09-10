b203fb5f173d3f86a2eb4c8ed10b28d949b9beab
KarlJoones- <C13315991@mydit.ie>
2015-09-10 17:14:25 +0100

    V3.4 Revision 2

--- app/src/main/java/com/karl/android/coincounter/SettingsActivity.java
+++ app/src/main/java/com/karl/android/coincounter/SettingsActivity.java
@@ -6,8 +6,6 @@ import android.content.DialogInterface;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.media.audiofx.BassBoost;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,13 +16,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.Switch;
 
 import android.widget.Toast;
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Savepoint;
 import java.util.Arrays;
 
 
@@ -180,29 +174,6 @@ public class SettingsActivity extends AppCompatActivity {
         );
 
         myDB = new MySQLiteHelper(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        // Disabled because there is no need for settings here.
-        /*if (id == R.id.action_settings) {
-            return true;
-        } */
-
-        return super.onOptionsItemSelected(item);
     }
 
     /*
